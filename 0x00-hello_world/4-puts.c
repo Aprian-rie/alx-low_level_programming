00895eae75bf9f97e87ad52564d2993236357813
Apryan27 <aprythomas5@gmail.com>
2023-02-16 08:46:27 -0800

    Created an output using puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+
+int main()
+{
+		/*String Initializatton*/
+
+	char myStr[] = "Programming is like building a multilingual puzzle";
+		/*Print The String using Puts*/
+
+	puts(myStr);
+
+	return 0;
+}
