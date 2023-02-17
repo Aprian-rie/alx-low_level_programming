a9b062b110c45acc523073adec334f7289d51a77
Apryan27 <aprythomas5@gmail.com>
2023-02-16 17:41:54 -0800

    write function to print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,7 @@ int main(void)
 {
 	/*Using the function write to display the words*/
 	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
 	write(STDERR_FILENO, str, sizeof(str) - 1);
 	return (1);
 }
