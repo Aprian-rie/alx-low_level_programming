b446a1f40d8b73506a19186ea840caee0d4a09bb
Apryan27 <aprythomas5@gmail.com>
2023-02-17 12:51:00 -0800

    Skips letters

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+/**
+ * main - while usage
+ * Description: Print err except q and e
+ * Return: success (0)
+ */
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		if (ch == 'e' ||  ch == 'q')
+			ch = ch + 1;
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
