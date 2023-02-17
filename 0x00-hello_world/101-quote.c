9383f5051a9e86849b5398227a08b3f55d1b6709
Apryan27 <aprythomas5@gmail.com>
2023-02-16 17:39:24 -0800

    write function to print

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,10 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
+int main(void)
+{
 	/*Using the function write to display the words*/
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(STDERR_FILENO, str, sizeof(str) - 1);
 	return (1);
 }
