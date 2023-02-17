c1ddd033517a162089b817b0eb13c4ab65726995
Apryan27 <aprythomas5@gmail.com>
2023-02-17 10:20:03 -0800

    If else and if changes on improving codes adding

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
+/**
  * main - entry point
  * Description: Printing positive negative and zero
- * Return: always 0 (Success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
