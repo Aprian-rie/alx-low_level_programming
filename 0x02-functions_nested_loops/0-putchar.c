f4cb7a964121ecc7ccd03d2cfa1478a0d67745b3
Apryan27 <aprythomas5@gmail.com>
2023-02-21 11:45:59 -0800

    Prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,45 @@
+# include <stdio.h>
+/**
+ * main - prints putchar
+ * Description: using putchar to print putchar
+ * Return: 0 successful
+ */
+int main()
+{
+	int letter = '_';
+
+	putchar(letter);
+
+	letter = 'p';
+
+	putchar(letter);
+
+	letter = 'u';
+
+	putchar(letter);
+
+	letter = 't';
+
+	putchar(letter);
+
+	letter = 'c';
+
+	putchar(letter);
+
+	letter = 'h';
+
+	putchar(letter);
+
+	letter = 'a';
+
+	putchar(letter);
+
+	letter = 'r';
+
+	putchar(letter);
+
+	putchar('\n');
+
+	return (0);
+}
+
