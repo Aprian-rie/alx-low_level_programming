25e4127530ea30d4216ed446a0fa7087fcf160fe
Apryan27 <aprythomas5@gmail.com>
2023-02-21 15:59:42 -0800

    Adds 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+# include <stdio.h>
+/**
+ * add - adds
+ * Description: Adds 2 numbers
+ * @a: integer
+ * @b: integer
+ * Return: Sum
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * jack_bauer - 24
+ * Description: Counts the minutes
+ * Return: 0 success
+ */
+void jack_bauer(void)
+{
+
