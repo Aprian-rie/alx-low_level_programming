b32276204b013849471d8d7f63e30e14e91e2670
Aprian Bwire <aprythomas5@gmail.com>
2023-02-23 21:25:39 +0300

    Testing functions follow instructions

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,16 @@
-#include <stdio.h>
 #include "main.h"
+
 /**
- * main - tests functions
- * Description: It tests functions
- * Return: nothing it's void
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
 int main(void)
 {
-	int i;
+        int i;
 
-	i = 0;
-	positive_or_negative(i);
-	return (0);
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
 }
