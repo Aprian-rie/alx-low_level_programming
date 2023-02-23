fda781d3387d22b312e68a9ba9a23eee3d4c2d67
Aprian Bwire <aprythomas5@gmail.com>
2023-02-23 21:17:09 +0300

    Testing function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - tests functions
+ * Description: It tests functions
+ * Return: nothing it's void
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+	return(0);
+}
