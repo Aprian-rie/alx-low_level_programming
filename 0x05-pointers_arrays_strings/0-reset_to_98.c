ec91e3e4c8792050c912441c8dde9906a14d5aff
Apryan27 <aprythomas5@gmail.com>
2023-02-27 10:00:36 -0800

    Sets the value to 98 on another function

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * reset_to_98 - Sets the value of int to 98
+ * Description: Refer above
+ * @*n - the value that replaces
+ * Return: Nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,5 +2,6 @@
 #define FILE_MAIN
 
 int _putchar(char c);
+void reset_to_98(int *n);
 #endif
 
