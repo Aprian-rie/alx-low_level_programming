68544a3f5c7e0242483dc7562ec0e1a6a9cb18fe
Apryan27 <aprythomas5@gmail.com>
2023-02-27 10:03:42 -0800

    Added description for parameter

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 /**
  * reset_to_98 - Sets the value of int to 98
  * Description: Refer above
- * @n - the value that replaces
+ * @n: the value that replaces
  * Return: Nothing
  */
 void reset_to_98(int *n)
