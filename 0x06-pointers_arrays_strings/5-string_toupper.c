409abd9653a5abe1e7352c2f78089bf139481f6c
Aprian Bwire <aprythomas5@gmail.com>
2023-03-03 02:34:20 +0300

    Fixed betty errors

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,13 +1,14 @@
 # include <stdio.h>
 # include "main.h"
 /**
- * _toupper - converts all lowercase letters to uppercase
+ * string_toupper - converts all lowercase letters to uppercase
  * Return: A character
+ * @s: the character to be converted
  */
 char *string_toupper(char *s)
 {
-	for(; *s; s++)
-		if(('a' <= *s) && (*s <= 'z'))
+	for (; *s; s++)
+		if (('a' <= *s) && (*s <= 'z'))
 			*s = 'A' + (*s - 'a');
 	return (s);
 }
