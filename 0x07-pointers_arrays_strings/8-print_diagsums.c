afbd44d77e2c419ddba1f22798b03211eef21028
Aprian Bwire <aprythomas5@gmail.com>
2023-03-06 15:45:30 +0300

    sum of diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+#include "main.h"
+/**
+ * print_diagsums - prints the sum of the two diagonal of a square matrix of integers
+ * @a: the elements
+ * @size: size of the matrix
+ * Return: none
+ */
+void print_diagsums(int *a, int size)
+{
+
