a9c8d7027997bc3212d39d928bed1ea19777a220
Aprian Bwire <aprythomas5@gmail.com>
2023-03-14 01:50:33 +0300

    strcpy

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,9 +1,23 @@
-# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
 /**
  * _strdup - returns a pointer
- * @str - a string
+ * @str:  a string
  * Return: null of str = NULL
  */
 char *_strdup(char *str)
 {
-	
+	char *dup_str;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	dup_str = malloc(strlen(str) + 1);
+	if (dup_str == NULL)
+	{
+		return (NULL);
+	}
+	strcpy(dup_str, str);
+	return (dup_str);
+}
