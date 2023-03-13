c578ff9eb7bb8edb370c2f3cb2ae327578b521c0
Aprian Bwire <aprythomas5@gmail.com>
2023-03-14 01:42:30 +0300

    One final push bugs + Betty!

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -7,14 +7,16 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *arr = malloc (sizeof(char) * size);
+	char *arr = malloc(sizeof(char) * size);
 	unsigned int i;
 
 	if (size == 0)
+		return (NULL);
+	if (arr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
-	return arr;
+	return (arr);
 }
