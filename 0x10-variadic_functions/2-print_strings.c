10ada2f0595fb1f484ede987d9abcc7a91407708
Aprian Bwire <aprythomas5@gmail.com>
2023-03-26 15:18:33 +0300

    fixed the main file

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdargs.h>
+#include <stdarg.h>
 /**
  * print_strings - it print strings
  * @separator: strings printed between the strings
@@ -9,6 +9,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+	char *str;
 
 	va_start(args, n);
 	for (i = 0; i < n; i++)
