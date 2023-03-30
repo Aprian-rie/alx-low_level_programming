748160c24a948ee9ab5ec5f853f395ec98c58dd7
Aprian Bwire <aprythomas5@gmail.com>
2023-03-31 01:55:25 +0300

    Assembly language

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,16 @@
+global main
+extern printf
+
+section .data
+	hello: db 'Hello, Holberton', 0
+	format: db '%s', 10, 0
+
+section .text
+main:
+	push hello
+	push format
+	call printf
+	add rsp, 16
+
+	mov eac, 0
+	ret
