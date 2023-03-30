f0ca07bb516dd038d70a7fc9ed031140c1891229
Aprian Bwire <aprythomas5@gmail.com>
2023-03-31 02:12:03 +0300

    Usage of free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_list - frees a list_t list
+ * @head: a pointer to the head of the list
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node;
+
+	while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
