173f080a6dc7e36c782ee5844a69688eafd25103
Aprian Bwire <aprythomas5@gmail.com>
2023-03-30 22:51:36 +0300

    finds number of elements

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * list_len - returns number of elements
+ * @h:  pointer to the head of the list
+ * Return: number of elements
+ */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
