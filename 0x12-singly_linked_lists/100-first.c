5eb75b6122a4574726161a89066653b5f1e773fe
Aprian Bwire <aprythomas5@gmail.com>
2023-03-31 01:51:04 +0300

    prints before main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_message - prints a message before main
+ * print_message - a function
+ * Return: void
+ */
+void print_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
