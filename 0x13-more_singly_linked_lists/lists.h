387733d30e7c18846b42c48ac3d6a9e60606965d
Aprian Bwire <aprythomas5@gmail.com>
2023-04-03 08:24:10 +0300

    Added a header file

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,18 @@
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+#include <stdlib.h>
+#ifndef LISTS_H
+#define LISTS_H
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+}listint_t;
+size_t print_listint(const listint_t *h);
+#endif
