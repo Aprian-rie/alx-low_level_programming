dbe38a77ca71fef3acf4ab61915d86bc3b811153
Aprian Bwire <aprythomas5@gmail.com>
2023-04-04 19:15:37 +0300

    Added a prototype in the header

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer pointing to the head of the node
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+
