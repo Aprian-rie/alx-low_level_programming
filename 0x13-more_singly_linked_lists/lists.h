d372037e04d4036b45863b8f9876ba690f249200
Aprian Bwire <aprythomas5@gmail.com>
2023-04-04 18:07:30 +0300

    Fixed header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,5 @@
+#ifndef LISTS_H
+#define LISTS_H
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -6,14 +8,11 @@
  * Description: singly linked list node structure
  *
  */
-#include <stdlib.h>
-#ifndef LISTS_H
-#define LISTS_H
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
