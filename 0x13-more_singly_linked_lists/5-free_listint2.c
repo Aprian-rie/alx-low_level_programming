927c22e7ed8c488010f97aae1bd3cbd5c9eeabd0
Aprian Bwire <aprythomas5@gmail.com>
2023-04-04 19:45:46 +0300

    Frees a list- double pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - frees a list again
+ * @head: a pointer to a pointer that points the head of the node
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *ptr;
+
+	while (*head != NULL)
+	{
+		ptr = *head;
+		*head = (*head)->next;
+		free(ptr);
+	}
+}
