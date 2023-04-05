a22c5ef7231e55ae1e6024dcf293cd7eb4e0200a
Aprian Bwire <aprythomas5@gmail.com>
2023-04-06 00:18:26 +0300

    frees the linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,5 +9,6 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 
+
 	return (count);
 }
---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,12 @@
+#include <stlib.h>
+#include "lists.h"
+/**
+ * free_listint_safe - frees a listint_t list
+ * @h: a pointer that points to a pointer
+ * Return: size of list freed
+ */
+size_t free_listint_safe(listint-t **h)
+{
+	size_t size = 0;
+	return (size);
+}
