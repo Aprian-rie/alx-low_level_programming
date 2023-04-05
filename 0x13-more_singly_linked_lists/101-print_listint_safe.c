d4b95ba0833c1103f46562c0a9ed400320ee3ba6
Aprian Bwire <aprythomas5@gmail.com>
2023-04-06 00:09:49 +0300

    prints a linked list

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * print_listint_safe - prints a linked list
+ * @head: a pointer pointing to the head
+ * Return: number of nodes
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	return (head);
+}
