fc90212cff5b83dc1b7d4e0867685595dd493817
Aprian Bwire <aprythomas5@gmail.com>
2023-04-09 01:13:15 +0300

    flipping bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,19 @@
+/**
+ * flip_bits - returns no of bits you'd need to flip to get from on no to another
+ * @n: the first number
+ * @m: the second number
+ * Return: number of bits
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int count = 0;
+	unsigned int result;
+
+	result = n ^ m;
+	while (result != 0)
+	{
+		count += result & 1;
+		result >>= 1;
+	}
+	return (count);
+}
