d6f49064b72485ca693a47a471907ffa76cbbbbf
Aprian Bwire <aprythomas5@gmail.com>
2023-04-08 22:20:15 +0300

    modified the conversion.

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,21 +8,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0;
-	int length = strlen(b);
-	unsigned int weight = 1;
 	int i;
 
-	if (b == NULL || b[0] == '\0')
+	if (b == NULL)
+	{
 		return (0);
-	for (i = (length - 1); i >= 0; i--)
+	}
+	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] == '1' || b[i] == '0')
 		{
-			if (b[i] == '1')
-			{
-				result = result + weight;
-			}
-		weight = weight * 2;
+			result = result << 1;
+			result += b[i] - '0';
 		}
 		else
 			return (0);
