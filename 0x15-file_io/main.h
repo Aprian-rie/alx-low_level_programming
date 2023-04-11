b027ec976ff5bdb625b7c96eb7522fb97e09b834
Aprian Bwire <aprythomas5@gmail.com>
2023-04-11 19:59:11 +0300

    removed header files

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,16 +3,4 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-/**
- * _error - prints error message
- * @msg: the intended message
- * @file: the filename
- * @code: the code for error
- * Return: nothing
- */
-void _error(char *msg, char *file, int code)
-{
-	dprintf(STDERR_FILENO, msg, file);
-	exit(code);
-}
 #endif
